32e3705ac719b024a60042d3b5651c43db2b4772
Samuel-Brr <samuel.barre972@gmail.com>
2022-07-25 15:17:30 +0200

    quete

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,28 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable String number) {
 
-        return null;
+        if(number.equals("13")){
+            return new Doctor(13, "Jodie Whittaker");
+        }
+
+        if(Integer.parseInt(number) > 0 && Integer.parseInt(number) < 13){
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "see other");
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation" + number);
     }
 }
